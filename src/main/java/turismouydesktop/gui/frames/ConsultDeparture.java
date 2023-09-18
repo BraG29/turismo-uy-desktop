49758e955d4f4fb28112d4a99a9e75d44e5f6658
BraG29 <braianbarrios_29@hotmail.com>
2023-09-17 23:55:56 -0300

    Fix ConsultDeparutre

--- src/main/java/turismouydesktop/gui/frames/ConsultDeparture.java
+++ src/main/java/turismouydesktop/gui/frames/ConsultDeparture.java
@@ -13,11 +13,13 @@ import uy.turismo.servidorcentral.logic.controller.ControllerFactory;
 import uy.turismo.servidorcentral.logic.controller.IController;
 import uy.turismo.servidorcentral.logic.datatypes.DtDepartment;
 import uy.turismo.servidorcentral.logic.datatypes.DtTouristicActivity;
+import uy.turismo.servidorcentral.logic.datatypes.DtTouristicBundle;
 import uy.turismo.servidorcentral.logic.datatypes.DtTouristicDeparture;
 
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.AbstractListModel;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -26,15 +28,23 @@ import java.awt.event.ActionEvent;
 import javax.swing.JList;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.ListSelectionEvent;
+import javax.swing.JComboBox;
 
 public class ConsultDeparture extends JFrame implements ListDepartmentListener{
 
 	private JPanel contentPane;
-	private ShowDepartureData departureData;
+	private ShowDepartureData departureDataPanel;
 	
 	private ListDepartment departmentsList; //panel
 	private JList listDepartmentsActivities;
+	
 	private List<DtTouristicActivity> activities;
+	private List<DtTouristicDeparture> departures;
+	
+	private JComboBox comboBoxDepartures;
+	
+	private String[] emptySet = {"-"};
+	
 	
 
 	/**
@@ -50,9 +60,9 @@ public class ConsultDeparture extends JFrame implements ListDepartmentListener{
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		departureData = new ShowDepartureData();
-		departureData.setBounds(268, 28, 300, 224);
-		contentPane.add(departureData);
+		departureDataPanel = new ShowDepartureData();
+		departureDataPanel.setBounds(264, 69, 300, 224);
+		contentPane.add(departureDataPanel);
 		
 		departmentsList = new ListDepartment();
 		departmentsList.setBounds(12, 45, 182, 113);
@@ -78,19 +88,62 @@ public class ConsultDeparture extends JFrame implements ListDepartmentListener{
 							
 				String selectedActivity = (String) listDepartmentsActivities.getSelectedValue();
 				
-				Long activityId = activities.stream().filter(departure -> departure.getName().equals(selectedActivity))
+				Long activityId = activities
+						.stream()
+						.filter(activity -> activity.getName().equals(selectedActivity))
 						.findFirst()
 						.get()
 						.getId();
 				
 				IController ctrl = ControllerFactory.getIController();
-				DtTouristicDeparture departure = ctrl.getTouristicDepartureData(activityId);
-				departureData.loadData(departure);
+				departures = ctrl.getListTouristicDeparture(activityId);
+				loadDepartures();
 				
 			}
 		});
 		scrollPaneActDep.setViewportView(listDepartmentsActivities);
 		
+		comboBoxDepartures = new JComboBox();
+		comboBoxDepartures.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String selectedDeparture = (String) comboBoxDepartures.getSelectedItem();
+				
+				if(comboBoxDepartures.getSelectedIndex() != 0) {
+					Long departureId = departures
+							.stream()
+							.filter(deparutre -> deparutre.getName().equals(selectedDeparture))
+							.findFirst()
+							.get()
+							.getId();
+					
+					IController controller = ControllerFactory.getIController();
+					
+					DtTouristicDeparture departureData = controller.getTouristicDepartureData(departureId);
+					
+					departureDataPanel.loadData(departureData);
+				}
+			}
+		});
+		comboBoxDepartures.setBounds(343, 23, 215, 24);
+		comboBoxDepartures.setModel(new DefaultComboBoxModel(emptySet));
+		contentPane.add(comboBoxDepartures);
+		
+	}
+	
+	public void loadDepartures() {
+		
+		String[] departuresString = departures.stream()
+                .map(DtTouristicDeparture::getName)
+                .collect(Collectors.toList())
+                .toArray(new String[0]);
+		
+		Integer totalLength = emptySet.length + departuresString.length;
+		
+		String[] concatenetedArray = new String[totalLength];
+		System.arraycopy(emptySet, 0, concatenetedArray, 0, emptySet.length);
+		System.arraycopy(departuresString, 0, concatenetedArray, emptySet.length, departuresString.length);
+		
+		comboBoxDepartures.setModel(new DefaultComboBoxModel(concatenetedArray));
 	}
 
 	@Override
@@ -129,8 +182,4 @@ public class ConsultDeparture extends JFrame implements ListDepartmentListener{
 		}
 		
 	}
-	
-	
-	
-	
 }
